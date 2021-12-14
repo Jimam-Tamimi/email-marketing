1a639347ca5f95c259dccd835ce8407bdc80e02c
jimam <jimamtamimi12@gmail.com>
2021-12-14 20:34:12 +0600

    pricing done

--- frontend/src/pages/home/Pricing.js
+++ frontend/src/pages/home/Pricing.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../../styles/Essentials.styles'
-import { BuyNow, HeroHeading, HeroWrap, PlanDesc, PlanHeading, PlanLink, PlanOffer, PlanOffersHeading, PlanPrice, PlanStarts, PricingColumn, PricingContainer, PricingGrid, PricingWrap,   } from '../styles/home/Pricing.styles'
+import { BuyNow, CompanyLink, CompanyLogo, FaqAns, FaqColumn, FaqContainer, FaqDropDown, FaqDropDownAns, FaqDropDownWrap, FaqGrid, FaqHeading, FaqQuestion, FaqWrap, GoodCompanyColumn, GoodCompanyContainer, GoodCompanyGrid, GoodCompanyHeading, GoodCompanyWrap, HeroHeading, HeroWrap, PlanDesc, PlanHeading, PlanLink, PlanOffer, PlanOffersHeading, PlanPrice, PlanStarts, PricingColumn, PricingContainer, PricingGrid, PricingWrap,   } from '../styles/home/Pricing.styles'
 import {Link} from 'react-router-dom'
 import {BsCheckLg} from 'react-icons/bs'
+
+import companyLogo1 from '../../assets/images/company-logo-1.webp'
+import companyLogo2 from '../../assets/images/company-logo-2.webp'
+import companyLogo3 from '../../assets/images/company-logo-3.webp'
+import companyLogo4 from '../../assets/images/company-logo-4.webp'
+
 
 export default function Pricing() {
     return (
@@ -14,7 +20,7 @@ export default function Pricing() {
             <PricingWrap>
                 <PricingContainer>
                     <PricingGrid wrap="wrap">
-                        <PricingColumn lg={3}>
+                        <PricingColumn lg={3} md={6} sx={12}>
                             <PlanHeading>Premium</PlanHeading>
                             <PlanDesc>Advanced features for pros who need more customization.</PlanDesc>
                             <PlanStarts>Starts at</PlanStarts>
@@ -30,7 +36,7 @@ export default function Pricing() {
                             <PlanOffer><BsCheckLg style={{marginRight:"10px"}}  color='var(--color-primary)' />Phone & Priority Support</PlanOffer> 
                             <PlanLink style={{    marginTop: "15px"}} to="#">About Premium</PlanLink>
                         </PricingColumn> 
-                        <PricingColumn lg={3}>
+                        <PricingColumn lg={3} md={6} sx={12}>
                             <PlanHeading>Standard</PlanHeading>
                             <PlanDesc>Data-driven automation and optimization tools for businesses that want to grow faster.</PlanDesc>
                             <PlanStarts>Starts at</PlanStarts>
@@ -46,7 +52,7 @@ export default function Pricing() {
                             <PlanOffer><BsCheckLg style={{marginRight:"10px"}}  color='var(--color-primary)' />Dynamic Content</PlanOffer> 
                             <PlanLink style={{    marginTop: "15px"}} to="#">About Premium</PlanLink>
                         </PricingColumn> 
-                        <PricingColumn lg={3}>
+                        <PricingColumn lg={3} md={6} sx={12}>
                             <PlanHeading>Essentials</PlanHeading>
                             <PlanDesc>Great for email-only senders who want around-the-clock support.</PlanDesc>
                             <PlanStarts>Starts at</PlanStarts>
@@ -62,7 +68,7 @@ export default function Pricing() {
                             <PlanOffer><BsCheckLg style={{marginRight:"10px"}}  color='var(--color-primary)' />24/7 Email & Chat Support</PlanOffer> 
                             <PlanLink style={{    marginTop: "15px"}} to="#">About Premium</PlanLink>
                         </PricingColumn> 
-                        <PricingColumn lg={3}>
+                        <PricingColumn lg={3} md={6} sx={12}>
                             <PlanHeading>Free</PlanHeading>
                             <PlanDesc>All the multi-channel tools you need to build your business and grow your audience.</PlanDesc>
                             <PlanStarts>Starts at</PlanStarts>
@@ -81,6 +87,61 @@ export default function Pricing() {
                     </PricingGrid>
                 </PricingContainer>
             </PricingWrap>
+
+            <GoodCompanyWrap>
+                <GoodCompanyContainer >
+                    <GoodCompanyHeading>You’ll be in good <br /> company</GoodCompanyHeading>
+                    <GoodCompanyGrid wrap="wrap" justify="space-around" >
+                        <GoodCompanyColumn lg={3}  >
+                            <CompanyLink to="#"><CompanyLogo src={companyLogo1} /></CompanyLink>
+                        </GoodCompanyColumn>
+                        <GoodCompanyColumn lg={3}  >
+                            <CompanyLink to="#"><CompanyLogo src={companyLogo2} /></CompanyLink>
+                        </GoodCompanyColumn>
+                        <GoodCompanyColumn lg={3} >
+                            <CompanyLink to="#"><CompanyLogo src={companyLogo3} /></CompanyLink>
+                        </GoodCompanyColumn>
+                        <GoodCompanyColumn lg={3}  >
+                            <CompanyLink to="#"><CompanyLogo src={companyLogo4} /></CompanyLink>
+                        </GoodCompanyColumn>
+                    </GoodCompanyGrid>
+                </GoodCompanyContainer>
+                
+            </GoodCompanyWrap>
+
+
+            <FaqWrap>
+                <FaqContainer>
+                    <FaqHeading>Frequently asked questions</FaqHeading>
+                    <FaqGrid wrap="wrap" justify="space-around" >
+                        <FaqColumn lg={12}  ><DropDown /></FaqColumn>
+                    </FaqGrid>
+                </FaqContainer>
+            </FaqWrap>
+            
+        </>
+    )
+}
+
+ 
+
+function DropDown() {
+    const [showDropDown, setShowDropDown] = useState(false)
+
+    return (
+        <>
+            <FaqDropDownWrap>
+                <FaqDropDown onClick={() => setShowDropDown(!showDropDown)}>
+                    <FaqQuestion>What payment methods do you offer?</FaqQuestion>
+                </FaqDropDown>
+                {
+                    showDropDown && (
+                <FaqDropDownAns  >
+                    <FaqAns>We accept all major credit cards, PayPal, and (in select countries) direct debit. We also support transactions in a range of currencies.</FaqAns>
+                </FaqDropDownAns>
+                    )
+                }
+            </FaqDropDownWrap>
         </>
     )
 }
--- frontend/src/pages/styles/home/Pricing.styles.js
+++ frontend/src/pages/styles/home/Pricing.styles.js
@@ -34,6 +34,7 @@ export const PricingColumn = styled(Column)`
     align-items: flex-start;
     padding: 50px 40px;
     border: 1px solid #dbd9d2;
+    margin: 20px 0px;
 `
 export const PlanHeading = styled.h2`
 font-size: 1.89rem;
@@ -62,7 +63,7 @@ export const PlanPrice = styled.h1`
         content: '$';
         font-size: .9rem;
         margin-right: 5px;
-        
+
     }
     
 `
@@ -79,4 +80,74 @@ export const PlanOffersHeading = styled.h6`
 export const BuyNow = styled(Button)`
     width: 100%;
     margin: 30px 0px;
+`
+
+
+
+export const GoodCompanyWrap = styled.section`
+    padding: 100px 0px;
+
+`
+export const GoodCompanyContainer = styled(Container)`
+
+    ${Flex}
+    flex-direction: column;
+    align-items: center;
+`
+export const GoodCompanyGrid = styled(Grid)`
+
+`
+export const GoodCompanyHeading = styled.h1`
+    text-align: center;
+    font-size: 3.2rem;
+    font-weight: 500;
+    margin: 50px 0px;
+
+
+`
+export const GoodCompanyColumn = styled(Column)`
+
+`
+export const CompanyLink = styled(Link)`
+
+`
+export const CompanyLogo = styled.img`
+    width: 80%;
+`
+
+
+
+
+export const FaqWrap = styled.section`
+    
+`
+export const FaqHeading = styled.h1`
+    
+`
+export const FaqContainer = styled(Container)`
+    ${Flex}
+    flex-direction: column;
+`
+export const FaqGrid = styled(Grid)`
+    
+`
+
+export const FaqColumn = styled(Column)`
+    
+`
+export const FaqDropDownWrap = styled.div`
+width: 100%;
+    
+`
+export const FaqDropDown = styled.div`
+
+`
+export const FaqQuestion = styled.h2`
+
+`
+export const FaqDropDownAns = styled.div`
+
+`
+export const FaqAns = styled.p`
+
 `
